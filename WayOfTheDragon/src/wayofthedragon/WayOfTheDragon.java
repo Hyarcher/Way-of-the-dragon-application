713662105f4543c1b6d4c2bd3f19ced96685259a
Hyarcher <HyarcherHyarcher@gmail.com>
2019-02-10 15:57:32 +0000

    Project initiation
    Created base files for the project

---
+++ WayOfTheDragon/src/wayofthedragon/WayOfTheDragon.java
@@ -0,0 +1,17 @@
+
+package wayofthedragon;
+
+/**
+ *
+ * @author Up839653
+ */
+public class WayOfTheDragon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
