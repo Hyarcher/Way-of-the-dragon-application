6dcba99a297255713e90094f6a4fdff5f6341952
gwhizzle <georgeavery28@gmail.com>
2019-03-10 13:38:46 +0000

    Add files via upload
    Added GUIs and added some buttons to change forms

---
+++ Way-of-the-dragon-application-master/WayOfTheDragon/src/wayofthedragon/WayOfTheDragon.java
@@ -0,0 +1,18 @@
+
+package wayofthedragon;
+
+/**
+ *
+ * @author Up839653
+ */
+public class WayOfTheDragon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login_Page login = new Login_Page();
+        login.setVisible(true);
+    }
+    
+}
